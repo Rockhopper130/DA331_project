877111c6f178fb48e1df7f423d13b8d47b7d6f51
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2023-11-30 23:20:50 +0530

    dark-mode--with--minor-changes

--- src/pages/DashBord.js
+++ src/pages/DashBord.js
@@ -16,6 +16,8 @@ function DashBord() {
   const [loading, setLoading] = useState(true);
   const [reviews, setReviews] = useState([]);
   const [suggestions, setSuggestions] = useState([]);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
 
   useEffect(() => {
     const getRandomReviews = (array, count) => {
@@ -26,6 +28,16 @@ function DashBord() {
     const twoRandomReviews = getRandomReviews(MoviesReviews, 2);
     setReviews(twoRandomReviews);
   }, [loading]);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
 
   async function search() {
     try {
@@ -144,17 +156,17 @@ function DashBord() {
   }, [searchedmovie]);
   // console.log(data)
   return (
-    <div className=" flex flex-col ">
+    <div className=" flex flex-col bg-white dark:bg-black dark:text-white ">
       <nav className="flex flex-col lg:flex lg:flex-row  p-4 justify-between ">
         <h1 className=" text-3xl font-bold text-amber-400">Movie-Analysis</h1>
         <div className="flex flex-col relative">
-  <div className="flex w-[100%] mr-64 relative items-center">
+  <div className="flex w-[100%]  -mr-10 ml-64 relative items-center">
     <input
       onChange={(e) => {
         const updatedValue = e.target.value.replace(/\s+/g, "+");
         setsearchedmovie(updatedValue);
       }}
-      className="flex-1 px-3 border-2 rounded-xl border-grey py-2 mr-5 outline-none w-[40%]"
+      className="flex-1 px-3 text-black  border-2 rounded-xl border-grey py-2 mr-5 outline-none w-[40%]"
       placeholder="Search Movies..."
     />
     <img
@@ -164,14 +176,47 @@ function DashBord() {
       alt="Search Icon"
     />
   </div>
-  <ul className="absolute mt-10 ">
+  <ul className="absolute mt-10 ml-64 ">
     {suggestions.map((suggestion, index) => (
-      <li key={index} className="border px-3 cursor-pointer rounded-md bg-[#F7F7F7] py-2" onClick={() => handleSuggestionClick(suggestion)}>
+      <li key={index} className="border px-3 dark:bg-black cursor-pointer rounded-md bg-[#F7F7F7] py-2" onClick={() => handleSuggestionClick(suggestion)}>
         {suggestion}
       </li>
     ))}
   </ul>
 </div>
+
+
+
+<div className="flex  flex-col justify-center items-center cursor-pointer">
+ 
+      <div className="flex justify-center items-center">
+       
+        <span className="">
+        <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+    </span>
+        {/* Switch Container */}
+        <div
+          className={`w-14 h-7 flex items-center bg-gray-300 rounded-full mx-3 px-1 ${
+            isDarkMode ? 'bg-cyan-700' : ''
+          }`}
+          onClick={toggleDarkMode}
+        >
+          {/* Switch */}
+          <div
+            className={`bg-white w-5 h-5 rounded-full shadow-md transform ${
+              isDarkMode ? 'translate-x-7' : ''
+            }`}
+          ></div>
+        </div> <span className="">
+        <svg className="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+        </svg>
+    </span>
+      </div>
+      
+    </div>
 
       </nav>
       <div className=" flex justify-end -pt-5 pr-[18%]">
@@ -190,7 +235,7 @@ function DashBord() {
           )}
           <div>
             <h1 className="text-2xl font-bold">{Title}</h1>
-            <h2 className=" text-[#161513] items-center text-sm">
+            <h2 className=" text-[#161513] dark:text-white items-center text-sm">
               {Year} | {Genre}
             </h2>
             <h2 className=" text-[#3C9014] flex font-semibold items-center">
@@ -198,16 +243,17 @@ function DashBord() {
               {Rating_count})
             </h2>
             <div className="flex gap-[60%] ml-5 mt-4 mb-2">
-              <h2 className=" font-bold text-[#161513]  text-sm ">
+              <h2 className=" dark:text-white font-bold text-[#161513]  text-sm ">
                 Top Reviews
               </h2>
 
-              <img className=" cursor-pointer" src="/right-arrow.svg" />
+             {!isDarkMode? <img className=" cursor-pointer" src="/right-arrow.svg" />:<img className=" cursor-pointer" src="/right-arrow-white.svg" />}
             </div>
-            <div className=" bg-[#F7F7F7] mt-3 flex flex-col gap-4">
+            <div className=" bg-[#F7F7F7] py-3 dark:bg-black mt-3 flex flex-col gap-4">
               {reviews.map((data) => (
                 <div className="">
-                  <img src="/inverted.svg" />
+               { isDarkMode?  <img src="/inverted-white.svg" />:
+                  <img src="/inverted.svg" />}
                   <div className="w-[100%] pl-4 pt-2">{data.comment}</div>
                   <div className="w-[100%]  flex justify-between">
                     <div className="ml-5 text-[#3C9014] flex items-center">
@@ -215,9 +261,12 @@ function DashBord() {
                       <img src="/star.png" className=" w-4 h-4" />
                       {data.rating}/5
                     </div>
-                    <div className="text-[#4DA8EA] bg-[#E0F2FF] px-2 py-1 font-bold rounded-md">
-                      Happy
+                  <div className="flex gap-2 mr-9"> <div className="text-[#32EE66] text-sm bg-[#D2FFD6] px-2 py-[3px] font-semibold rounded-md ">
+                      Funny
                     </div>
+                    <div className="text-[#4DA8EA] text-sm bg-[#E0F2FF] px-2 py-[3px] font-semibold rounded-md">
+                      Happy
+                    </div></div> 
                   </div>
                 </div>
               ))}
@@ -231,8 +280,8 @@ function DashBord() {
           <div
             onClick={() => handleSuggestionClick(data.Title)}
             className="mx-5 cursor-pointer bg-[#F7F7F7]"
-          >
-            <div className="flex gap-2 px-10 py-2 border">
+          > 
+            <div className="flex dark:bg-black  gap-2 px-10 py-2 border">
               {loading ? (
                 <div className="animate-pulse">
                   <div className="bg-gray-400 h-56 w-40 rounded-md mb-4"></div>
@@ -245,13 +294,19 @@ function DashBord() {
                   alt={data.Title}
                 />
               )}
-              <div className="overflow-hidden">
-                <h1 className="whitespace-nowrap text-sm font-bold">
+              <div className=" flex flex-col justify-between">
+              <div>  <h1 className="whitespace-nowrap text-sm font-bold">
                   {data.Title}
                 </h1>
-                <h2 className="whitespace-nowrap text-[#161513] items-center text-xs">
+                <h2 className="whitespace-nowrap dark:text-white text-[#161513] items-center text-xs">
                   {data.Year}
-                </h2>
+                </h2></div>
+                <div className="flex gap-2 mr-9 mb-5"> <div className="text-[#32EE66] text-sm bg-[#D2FFD6] px-2 py-[3px] font-semibold rounded-md ">
+                      Funny
+                    </div>
+                    <div className="text-[#4DA8EA] text-sm bg-[#E0F2FF] px-2 py-[3px] font-semibold rounded-md">
+                      Happy
+                    </div></div> 
               </div>
             </div>
           </div>
